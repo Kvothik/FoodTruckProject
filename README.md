cb49cb63ec4fbfb49348b2599f1248b783de19c6
Kvothik <cromeans15@gmail.com>
2019-09-21 21:54:34 -0600

    second upload

--- README.md
+++ README.md
@@ -3,7 +3,28 @@
 ### Week 2 Homework Project for Skill Distillery
 
 ### Overview
+FoodTruck project was created using 4 classes. I will break down what each class does.
 
+#### 1. FoodTruck
+FoodTruck has fields for the FoodTruck object:
+1. -truckName
+2. -foodType
+3. -rating
+4. -truckID
+
+Getters and setters are created for each of the fields along with a default constructor and constructor using the four fields.
+
+FoodTruck class has two additional methods:
+1. toString
+toString method returns a concatenated String with the FoodTrucks data.
+2. ratingStars
+ratingStars returns a star emoji based on how many stars the review has given said truck.
+
+#### 2. FoodTruckTester
+
+
+#### 3. Menu
+#### 4. FoodTruckData
 ### Technologies/Topics Applied
 
 ### Lessons Learned
--- src/com/skilldisillery/foodtrucks/FoodTruck.java
+++ src/com/skilldisillery/foodtrucks/FoodTruck.java
@@ -1,5 +1,83 @@
 package com.skilldisillery.foodtrucks;
 
 public class FoodTruck {
+	private static int truckID;
+	private String truckName;
+	private String foodType;
+	private int rating;
 
+	public FoodTruck() {
+		super();
+	}
+
+	public FoodTruck(int truckID, String truckName, String foodType, int rating) {
+		{
+			FoodTruck.truckID = truckID;
+		}
+
+		this.truckName = truckName;
+		this.foodType = foodType;
+		this.rating = rating;
+
+	}
+
+	public int getTruckID() {
+		return truckID;
+	}
+
+	public void setTruckID(int truckID) {
+		FoodTruck.truckID = truckID;
+	}
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString() {
+		return "Truck ID #" + truckID + "\tTruck name: " + truckName + "\t Type of food: " + foodType + "\t Rating: "
+				+ ratingStars(rating);
+	}
+
+	public String ratingStars(int rating) {
+		String ratingStars = "";
+		switch (this.rating) {
+		case 1:
+			ratingStars = "\u2b50";
+			return ratingStars;
+		case 2:
+			ratingStars = "\u2b50\u2b50";
+			return ratingStars;
+		case 3:
+			ratingStars = "\u2b50\u2b50\u2b50";
+			return ratingStars;
+		case 4:
+			ratingStars = "\u2b50\u2b50\u2b50\u2b50";
+			return ratingStars;
+		case 5:
+			ratingStars = "\u2b50\u2b50\u2b50\u2b50\u2b50";
+			return ratingStars;
+		}
+		return ratingStars;
+	}
 }
